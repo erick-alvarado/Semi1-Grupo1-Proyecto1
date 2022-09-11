16fe27054aeb91d0d3f52e2ad3d828199d910bab
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2022-09-11 00:30:56 -0600

    Agregar Amigos

--- Frontend/front-end/src/page/AgregarAmigos.js
+++ Frontend/front-end/src/page/AgregarAmigos.js
@@ -11,8 +11,7 @@ export const AgregarAmigos = () => {
       <div class="split left">
       <Nav/>
     </div>
-    <div class="split right"></div>
-
+    <div class="split right">
       <div className="container_AA">
       <div className='AA_text_VA'>
       <label htmlFor="uname" style={{color:"BLACK" } }><b>BUSCAR USUARIO</b></label>
@@ -20,22 +19,27 @@ export const AgregarAmigos = () => {
       <div>
       <button className='AA_button_VA'>BUSCAR</button>
       </div>
-      <div className='container_AA_view'>
+      
         <h3>Todos los amigos</h3>
       <div className={"container3"} >
         {
               window.users.map((index)=>{
+                if(window.user != index.idUser){
                   return(
-                  <div className={"item"}>
-                  <img className={"item-img"} src={index.foto} title = {index.user} alt="" />
-                  <div className={"item-text"}>
-                      <h3>{index.user}</h3>
-                  </div>
-                  </div>
-                  )
-
+                    <div className={"item"}>
+                    <img className={"item-img"} src={index.foto} title = {index.user} alt="" />
+                    <br/>
+                    <div className={"item-text"}>
+                        <h4>{index.user}</h4>
+                        <h5>{index.filespublic} archivos publicos </h5>
+                        <button type='submit'>Agregar Amigo</button>
+                    </div>
+                    </div>
+                    )
+                }
               })
             }
+    
     </div>
       </div>
       </div>
