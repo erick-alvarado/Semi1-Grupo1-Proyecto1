dbea07f9498f32fe5dc1ebc29096f2fe4ef8ff88
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2022-09-11 22:15:45 -0600

    Cambios bucket

--- Backend Python/config.py
+++ Backend Python/config.py
@@ -3,11 +3,12 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    S3_BUCKET_NAME="archivos-1-p1"
     S3_ACCESS_KEY_ID="AKIAXUSLBHHVFU4LXXBA"
     S3_SECRET_ACCESS_KEY="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"
-    S3_BUCKET_NAME="semibucket"
     S3_BUCKET_REGION="us-east-1"
     UPLOAD_PATH="/files2"
+    
 
     MYSQL_HOST="database-1.ct4jfvgo8r4m.us-east-1.rds.amazonaws.com"
     MYSQL_USER="admin"
